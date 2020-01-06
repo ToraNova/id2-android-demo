3b7519f951b6630f875ae3147706d5ac9586b38f
ToraNova <chia_jason96@live.com>
2020-01-07 06:48:18 +0800

    added preference settings

--- app/src/main/java/com/toranova/id2/Constant.java
+++ app/src/main/java/com/toranova/id2/Constant.java
@@ -2,5 +2,6 @@ package com.toranova.id2;
 
 public class Constant {
     public static final String mTag = "id2demo_d0";
+    public static final String mPrf = "id2demo_sp";
     //public static final String mTag = "VBLSProver";
 }
--- app/src/main/java/com/toranova/id2/MainActivity.java
+++ app/src/main/java/com/toranova/id2/MainActivity.java
@@ -3,7 +3,10 @@ package com.toranova.id2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.AsyncTask;
@@ -20,14 +23,11 @@ import android.widget.TextView;
 import com.toranova.id2.R;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -38,15 +38,22 @@ import java.util.Objects;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final String mTag = Constant.mTag;
-    private static final int OPEN_FILE = 2;
+    private static final int OPEN_USK = 2;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private int cpar = 2;
 
     private VBLSProver prover;
+    //deprecated, now import the params instead
     private String cparams[]  = new String[]{
-            "assets/param160.properties",
-            "assets/param224.properties",
-            "assets/param256.properties"
+        "assets/d192.param", // 0.2635ms; 0.02136ms; 0.2695ms
+        "assets/d256.param", // 0.3529ms; 0.03157ms; 0.3502ms
+        "assets/d359.param", // 0.7103ms; 0.05930ms; 0.7185ms
+        "assets/d407.param", // 0.8678ms; 0.08130ms; 0.9269ms
+        "assets/d522.param", // 1.522ms; 0.1323ms; 1.653ms
+        "assets/d677.param", // 2.440ms; 0.2242ms; 2.717ms
+        "assets/d1357.param" // 12.273ms; 0.9747ms; 14.91ms
     };
-
 
     private EditText idnt_in;
     private EditText host_in;
@@ -54,6 +61,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView res_out;
     private TextView key_out;
     private TextView key_file;
+    private TextView par_out;
     private Button trigger;
     private byte[] uskbuf;
 
@@ -71,15 +79,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         idnt_in = findViewById(R.id.uid_input);
         host_in = findViewById(R.id.host_input);
         port_in = findViewById(R.id.port_input);
         key_out = findViewById(R.id.dkey_view);
         key_file = findViewById(R.id.dkey_file);
         res_out = findViewById(R.id.result);
+        par_out = findViewById(R.id.par_out);
         trigger = findViewById(R.id.prove_button);
 
+        pref = getApplicationContext().getSharedPreferences(Constant.mPrf, MODE_PRIVATE);
+        String tmp = pref.getString("uskb64", null);
+        if(tmp == null){
+        }else{
+            byte[] hld = Base64.getDecoder().decode( tmp.getBytes() );
+            uskbuf = new byte[hld.length+1];
+            System.arraycopy(hld,0,uskbuf,0,hld.length);
+            uskbuf[hld.length] = 0x00; //append final 00
+            key_out.setText( tmp );
+        }
+
+        tmp = pref.getString("uskfilepath",null);
+        if(tmp == null){
+        }else{
+            key_file.setText( tmp );
+        }
+        cpar = pref.getInt("paramidx",2);
+        par_out.setText( cparams[cpar]);
         trigger.setOnClickListener(this);
 
     }
@@ -94,17 +120,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
-        case R.id.imp:
+        case R.id.key:
             //add the function to perform here
             //TODO: usk buffer from file
             // Request code for selecting a PDF document.
-            openFile(Uri.fromFile( new File(Environment.getExternalStorageDirectory().getAbsolutePath())) );
+            openFile(Uri.fromFile( new File(Environment.getExternalStorageDirectory().getAbsolutePath())), OPEN_USK );
+            return(true);
+
+        case R.id.par:
+            AlertDialog.Builder b = new AlertDialog.Builder(this);
+            b.setTitle("Select Curve Parameters dn");
+            String[] types = {"d192", "d256", "d359", "d407", "d522", "d677", "d1357"};
+            b.setItems(types, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    cpar = which;
+                    editor =  pref.edit();
+                    editor.putInt("paramidx", cpar);
+                    par_out.setText( cparams[cpar]);
+                    editor.apply();
+                }
+
+            });
+            b.show();
             return(true);
         }
         return(super.onOptionsItemSelected(item));
     }
 
-    private void openFile(Uri pickerInitialUri) {
+    private void openFile(Uri pickerInitialUri, int request) {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*");
@@ -112,26 +157,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Optionally, specify a URI for the file that should appear in the
         // system file picker when it loads.
         intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
-        startActivityForResult(intent, OPEN_FILE);
+        startActivityForResult(intent, request);
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if (resultCode == Activity.RESULT_OK) {
-
             try {
-                String uskb64 = readTextFromUri(data.getData());
-                //uskbuf = Base64.getDecoder().decode( uskb64.getBytes("UTF-8"));
-                byte[] hld = Base64.getDecoder().decode( uskb64.getBytes() );
-                uskbuf = new byte[hld.length+1];
-                for(int i=0;i<hld.length;i++){
-                    uskbuf[i] = hld[i];
-                }
-                uskbuf[hld.length] = 0x00; //append final 00
-                //Log.i(mTag,"Read USK base64: "+ uskb64 );
-                key_out.setText(uskb64);
-                key_file.setText( data.getData().getPath() );
+                editor =  pref.edit();
+            switch(requestCode){
+                case OPEN_USK:
+                    String uskb64 = readTextFromUri(data.getData());
+                    editor.putString("uskb64", uskb64);  // Saving string
+                    //uskbuf = Base64.getDecoder().decode( uskb64.getBytes("UTF-8"));
+                    byte[] hld = Base64.getDecoder().decode( uskb64.getBytes() );
+                    uskbuf = new byte[hld.length+1];
+                    System.arraycopy(hld,0,uskbuf,0,hld.length);
+                    uskbuf[hld.length] = 0x00; //append final 00
+                    //Log.i(mTag,"Read USK base64: "+ uskb64 );
+                    key_out.setText(uskb64);
+                    key_file.setText( data.getData().getPath() );
+                    editor.putString("uskfilepath", data.getData().getPath());
+                    editor.apply();
+                    break;
+                default:
+                    //TODO: handle err here
+                    break;
+            }
+
             }catch(Exception e){
                 Log.e(mTag, "Exception caught:", e);
             }
@@ -169,16 +224,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if(uskbuf == null){
                 return 2;
             }
-            prover = new VBLSProver(cparams[2], pid, uskbuf);
-
-            byte[] rbuf = new byte[1024];
-            byte[] hld, sendID;
-            String hostname = args[0];
-            int port = Integer.parseInt(args[1]);
 
             try {
+                publishProgress("PROVE Begins");
+                prover = new VBLSProver( cparams[cpar], pid, uskbuf);
+
+                byte[] rbuf = new byte[1024];
+                byte[] hld, sendID;
+                String hostname = args[0];
+                int port = Integer.parseInt(args[1]);
                 int rc;
+
                 Log.i(mTag, "Connecting to " + hostname + ":" + port);
+                publishProgress("Connecting to " + hostname + ":" + port);
                 InetAddress serverAddr = InetAddress.getByName(hostname);
 
                 //create a socket to make the connection with the server
@@ -231,7 +289,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.i(mTag, "Identification Success (0x00)");
                 return 0;
             } catch( ConnectException e) {
-                Log.e(mTag, "Verifier Server not Up!");
+                Log.e(mTag, "Verifier Server not Up!",e);
                 return 3;
 
             }catch (Exception e) {
@@ -263,6 +321,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 case 3:
                     res_out.setText("Verifier Not Online 0x03");
                     break;
+                case 4:
+                    res_out.setText("Params not configured 0x04");
                 default:
                     res_out.setText("Unknown Error 0xFF");
                     break;
